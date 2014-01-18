3101e03738aed2b6b46abf060251e10b132581ba
rocknrollMarc <marc.bluemner@gmail.com>
2014-01-18 19:49:42 +0100

    user interface

--- features/support/world_extensions.rb
+++ features/support/world_extensions.rb
@@ -1,23 +1,28 @@
-#---
-# Excerpted from "The Cucumber Book",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material, 
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose. 
-# Visit http://www.pragmaticprogrammer.com/titles/hwcuc for more book information.
-#---
-module KnowsTheDomain
+module KnowsTheUserInterface
+
+  class UserInterface
+    include Capybara::DSL
+
+    def withdraw_from(account, amount)
+      visit '/'
+      fill_in 'Amount', :with => amount
+      click_button 'Withdraw'
+    end
+  end
+
   def my_account
     @my_account ||= Account.new
   end
-  
+
   def cash_slot
     @cash_slot ||= CashSlot.new
   end
-  
+
   def teller
-    @teller ||= Teller.new(cash_slot)
+    @teller ||= UserInterface.new
   end
+
 end
 
-World(KnowsTheDomain)
+World(KnowsTheUserInterface)
+
