ed946186bda2d48e55d5152fefae784d09b08803
rocknrollMarc <marc.bluemner@gmail.com>
2014-01-18 17:45:42 +0100

    initial commit

---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,64 @@
+class Account
+  def deposit(amount)
+    @balance = amount
+  end
+  
+  def balance
+    @balance
+  end
+end
+
+class Teller
+  def initialize(cash_slot)
+    @cash_slot = cash_slot
+  end
+  
+  def withdraw_from(account, amount)
+    @cash_slot.dispense(amount)
+  end
+end
+
+class CashSlot
+  def contents
+    @contents or raise("I'm empty!")
+  end
+  
+  def dispense(amount)
+    @contents = amount
+  end
+end
+
+CAPTURE_CASH_AMOUNT = Transform /^\$(\d+)$/ do |digits|
+  digits.to_i
+end
+
+module KnowsTheDomain
+  def my_account
+    @my_account ||= Account.new
+  end
+  
+  def cash_slot
+    @cash_slot ||= CashSlot.new
+  end
+  
+  def teller
+    @teller ||= Teller.new(cash_slot)
+  end
+end
+
+World(KnowsTheDomain)
+
+Given /^I have deposited (#{CAPTURE_CASH_AMOUNT}) in my account$/ do |amount|
+  my_account.deposit(amount)
+  my_account.balance.should eq(amount), 
+    "Expected the balance to be #{amount} but it was #{my_account.balance}"
+end
+
+When /^I withdraw (#{CAPTURE_CASH_AMOUNT})$/ do |amount|
+  teller.withdraw_from(my_account, amount)
+end
+
+Then /^(#{CAPTURE_CASH_AMOUNT}) should be dispensed$/ do |amount|
+  cash_slot.contents.should == amount
+end
+
