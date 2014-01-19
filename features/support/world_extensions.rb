0c8aa75a7a948fd6436f1f07fc3c803f8b2d8a2e
rocknrollMarc <marc.bluemner@gmail.com>
2014-01-19 12:40:45 +0100

    end chapter 8

--- features/support/world_extensions.rb
+++ features/support/world_extensions.rb
@@ -1,29 +1,23 @@
-module KnowsTheUserInterface
-
-  class UserInterface
-    include Capybara::DSL
-
-    def withdraw_from(account, amount)
-      Sinatra::Application.account = account
-      visit '/'
-      fill_in 'Amount', :with => amount
-      click_button 'Withdraw'
-    end
-  end
-
+#---
+# Excerpted from "The Cucumber Book",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/hwcuc for more book information.
+#---
+module KnowsTheDomain
   def my_account
     @my_account ||= Account.new
   end
-
+  
   def cash_slot
-    Sinatra::Application.cash_slot
+    @cash_slot ||= CashSlot.new
   end
-
+  
   def teller
-    @teller ||= UserInterface.new
+    @teller ||= Teller.new(cash_slot)
   end
-
 end
 
-World(KnowsTheUserInterface)
-
+World(KnowsTheDomain)
